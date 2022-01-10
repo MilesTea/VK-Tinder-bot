802c3d226e635a6b418c2e24e8afc13e738d1629
Dmitriy Ivanov <kurwatea@gmail.com>
2022-01-10 16:36:12 +0300

    Initial release

--- main.py
+++ main.py
@@ -2,9 +2,79 @@ from random import randrange
 import datetime
 import vk
 import vk_api
+import sql
 from vk_api.longpoll import VkLongPoll, VkEventType
 from pprint import pprint
 import requests
+
+
+"""keyboard = '''{
+    "one_time": false,
+    "inline": true,
+    "buttons": [
+        [
+        {
+        "action": {
+            "type": "text",
+            "label": "vladimir bruh",
+            "payload": null
+        },
+        "color": "primary"
+    }
+    ]
+    ]
+}'''
+keyboard = keyboard.replace(' ', '')
+keyboard = keyboard.replace('\n', '')
+print(keyboard)
+"""
+
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+main_keyboard = VkKeyboard(inline=True, one_time=False)
+main_keyboard.add_button('Очистка', color=VkKeyboardColor.SECONDARY)
+main_keyboard.add_button('Далее', color=VkKeyboardColor.PRIMARY)
+
+
+start_keyboard = VkKeyboard(inline=True, one_time=False)
+start_keyboard.add_button('Очистка', color=VkKeyboardColor.SECONDARY)
+start_keyboard.add_button('Поиск', color=VkKeyboardColor.PRIMARY)
+
+
+class VkLongPollImproved(VkLongPoll):
+    """
+    Попытка добавления очереди событий
+    """
+
+    def __init__(self, vk, wait=25, mode=vk_api.longpoll.DEFAULT_MODE,
+                 preload_messages=False, group_id=None):
+        super().__init__(vk, wait, mode, preload_messages, group_id)
+        self.queue = list()
+
+    def add_to_queue(self, event):
+        """
+        добавление события в очередь для последующей обработки
+        :param event: event
+        :return:
+        """
+        self.queue.append(event)
+
+    def improved_listen(self):
+        """
+        работает также, как и self.listen, за исключением того, что сначала обрабатываются события из очереди self.queue
+        :return: event
+        """
+        while True:
+            # print('queue')
+            if len(self.queue) >= 1:
+                # print('queue in')
+                for queued_event in self.queue:
+                    yield queued_event
+                self.queue.clear()
+            # print('not queue')
+            for event in self.check():
+                # print(event)
+                yield event
+
 
 params = {
     'bruh': 'bruh'
@@ -13,16 +83,13 @@ params = {
 token = '45bca88e142e2816d4e1bf4b2796efe5c19f2c46445c2fd4d11db4a652e51a7965f1ca576906cad6ce46e'
 
 vko = vk_api.VkApi(token=token)
-longpoll = VkLongPoll(vko)
+longpoll = VkLongPollImproved(vko)
 
 
 settings = {
     'age_range': 2,
 
 }
-
-def get_info(user_id):
-    return vko.method('users.get', {'user_id': user_id, 'fields': 'sex,bdate,city,country,relation'})
 
 
 def get_server_time() -> list[str]:
@@ -44,84 +111,160 @@ def age_meter(date1: list, date2: list) -> int:
         year -= 1
     return int(year)
 
-def search(params, settings):
-    age_from = params['age'] - settings['age_range']
-    age_to = params['age'] + settings['age_range']
-    if params['sex'] == 1: sex_params = 2
-    elif params['sex'] == 2: sex_params = 1
+
+def read_info(event):
+    info = vko.method('users.get', {'user_id': event.user_id, 'fields': 'sex,bdate,city,country,relation'})[0]
+    search_params = dict()
+    bdate, sex, city, occ = [None for i in range(4)]
+    if 'bdate' in info:
+        now = get_server_time()
+        bdate = info['bdate'].split('.')
+        if len(bdate) == 3:
+            age = age_meter(get_server_time(), bdate)
+            search_params['age'] = age
+        else:
+            write_msg(event.user_id, 'Введите ваш возраст')
+            search_params['age'] = int(wait_for_response(event.user_id))
+    else:
+        write_msg(event.user_id, 'Введите ваш возраст')
+        search_params['age'] = int(wait_for_response(event.user_id))
+    if 'sex' in info:
+        if info['sex'] in [1, 2]:
+            search_params['sex'] = info['sex']
+    else:
+        write_msg(event.user_id, 'Введите ваш пол\nМ\\Ж')
+        sex = wait_for_response(event.user_id)
+        if sex == 'м':
+            sex = 2
+        elif sex == 'ж':
+            sex = 1
+        if sex in [1, 2]:
+            search_params['sex'] = sex
+    if 'city' in info:
+        search_params['city'] = info['city']['id']
+    if 'relation' in info:
+        search_params['relation'] = info['relation']
+    # else:
+    pprint(search_params)
+    return search_params
+
+
+def search_users(event, search_params, settings):
+    age_from = search_params['age'] - settings['age_range']
+    age_to = search_params['age'] + settings['age_range']
+    if search_params['sex'] == 1: sex_params = 2
+    elif search_params['sex'] == 2: sex_params = 1
     else: sex_params = 0
     final_params = {'age_from': age_from, 'age_to': age_to, 'sex': sex_params}
-    final_params['city'] = params['city']
+    final_params['city'] = search_params['city']
     pprint(final_params)
     ids = vk.ids_from_users_search(vk.users_search(final_params))
     users_dict = dict()
     for id in ids:
         users_dict[id] = vk.get_photos(id)
-    return users_dict
+    for i, user in enumerate(users_dict):
+        message = f'https://vk.com/id{user}'
+        photos = list()
+        for attachment in users_dict[user]:
+            photos.append(attachment)
+        photos = ','.join(photos)
+        if photos:
+            write_msg(event.user_id, message, optional_params={'attachment': photos})
 
 
-def write_msg(user_id, message, optional_params=None):
+def search_user(event, search_params, settings, offset=0):
+    age_from = search_params['age'] - settings['age_range']
+    age_to = search_params['age'] + settings['age_range']
+    if search_params['sex'] == 1: sex_params = 2
+    elif search_params['sex'] == 2: sex_params = 1
+    else: sex_params = 0
+    final_params = {'age_from': age_from, 'age_to': age_to, 'sex': sex_params}
+    final_params['city'] = search_params['city']
+    pprint(final_params)
+    i = offset
+    print(i)
+    while True:
+        print(i)
+        while True:
+            print(i)
+            id = vk.id_from_users_search(vk.users_search(final_params, count=1, offset=i))
+            print(id)
+            i += 1
+            if not Db.check(id):
+                break
+        photos_list = vk.get_photos(id)
+        message = f'https://vk.com/id{id}'
+        photos = ','.join(photos_list)
+        if photos:
+            write_msg(event.user_id, message, keyboard=main_keyboard, optional_params={'attachment': photos})
+            Db.add(id)
+            return i
+        else:
+            i += 1
+
+
+'''
+    for i, user in enumerate(users_dict):
+        message = f'https://vk.com/id{user}'
+        photos = list()
+        for attachment in users_dict[user]:
+            photos.append(attachment)
+        photos = ','.join(photos)
+        if photos:
+            write_msg(event.user_id, message, optional_params={'attachment': photos})
+            Db.add(id[0])
+        else:
+'''
+
+
+def write_msg(user_id, message, keyboard=start_keyboard, optional_params=None):
     if optional_params is None:
         optional_params = {}
     vko.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': randrange(10 ** 7),
-                                 **optional_params})
+                                 'keyboard': keyboard.get_keyboard(), **optional_params})
+
 
 def wait_for_response(user_id):
     for new_event in longpoll.listen():
         if new_event.type == VkEventType.MESSAGE_NEW and new_event.to_me and new_event.user_id == user_id:
-            new_variable = new_event.text
+            new_variable = new_event.text.lower()
             return new_variable
+        else:
+            # print('added event')
+            longpoll.add_to_queue(new_event)
 
-waiting = False
-waiting_from = None
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
 
-        if event.to_me:
-            request = event.text.lower()
+def process(event):
+    pass
 
-            if request == "привет":
-                write_msg(event.user_id, f"Хай, {event.user_id}")
-            elif request == 'поиск':
-                bruh = search(search_params, settings)
-                pprint(bruh)
-                for i, user in enumerate(bruh):
-                    message = f'https://vk.com/id{user}'
-                    photos = list()
-                    for attachment in bruh[user]:
-                        photos.append(attachment)
-                    photos = ','.join(photos)
-                    if photos:
-                        write_msg(event.user_id, message, optional_params={'attachment': photos})
-            elif request == 'инфо':
-                info = get_info(event.user_id)[0]
-                search_params = dict()
-                bdate, sex, city, occ = [None for i in range(4)]
-                if 'bdate' in info:
-                    now = get_server_time()
-                    bdate = info['bdate'].split('.')
-                    if len(bdate) == 3:
-                        age = age_meter(get_server_time(), bdate)
-                        # bdate = datetime.date(int(bdate[2]), int(bdate[1]), int(bdate[0]))
-                        # age = get_server_time() - bdate
-                        # print(age)
-                        search_params['age'] = age
-                if 'sex' in info:
-                    search_params['sex'] = info['sex']
-                if 'city' in info:
-                    search_params['city'] = info['city']['id']
-                if 'relation' in info:
-                    search_params['relation'] = info['relation']
-                # else:
-                #     write_msg(event.user_id, 'Введите свой статус')
-                #     occ = wait_for_response(event.user_id)
-                us = search_params
-                print(us)
-                write_msg(event.user_id, us)
 
-            elif request == "пока":
-                write_msg(event.user_id, "Пока((")
-            elif request == 'тест':
-                print(type(get_server_time()))
-            else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+if __name__ == "__main__":
+    Db = sql.UsersDb()
+    readed = False
+    for event in longpoll.improved_listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            if event.to_me:
+                request = event.text.lower()
+
+                if request == "start":
+                    write_msg(event.user_id, f"Хай, {event.user_id}")
+                elif request == 'поиск':
+                    if not readed:
+                        search_params = read_info(event)
+                        readed = True
+                    offset = search_user(event, search_params, settings)
+                # elif request == 'инфо':
+                #     search_params = read_info(event)
+                elif request == "далее":
+                    print(offset)
+                    search_user(event, search_params, settings, offset=offset)
+                elif request == "очистка":
+                    Db.delete_all()
+                    write_msg(event.user_id, "Очистка выполнена")
+                elif request == "пока":
+                    write_msg(event.user_id, "Пока((")
+                elif request == 'тест':
+                    print(type(get_server_time()))
+                else:
+                    write_msg(event.user_id, "Не поняла вашего ответа...")
+                    print(request)
--- vk.py
+++ vk.py
@@ -1,71 +1,136 @@
 from pprint import pprint
 import requests
 import time
+token_url = 'https://oauth.vk.com/authorize?client_id=8039929&display=page&redirect_uri=https://oauth.vk.com/blank.html&response_type=token&v=5.131'
 
 application_id = '8039929'
 secret_key = '504Q0XHEYydfqbQ0kJ22'
 service_key = 'e8cac700e8cac700e8cac700a3e8b06af9ee8cae8cac70089072939b8972aff1b7c96ea'
-access_token = 'aca1367d5f73a5ebdc059bce045ccc2dc3f0bdce063f880bd207c49092ce1265089e9c62766845c766fa1'
+access_token = 'e576b3deb5a199aa609ceaee9a6dd1245c1667ca7914daf5fb6dee8709ab986db5d0ea9d9d9f44159a077'
 base_params = {
     'access_token': access_token,
     'v': '5.131'
 }
 
 base_url = 'https://api.vk.com/method/'
-def users_search(params):
-    full_params = {**params, **base_params,
+
+
+def users_search_old(search_params, offset=0, count=20):
+    full_params = {**search_params, **base_params,
                    'sort': '0',
-                   'fields': 'is_closed'
+                   'fields': 'is_closed',
+                   'count': count,
+                   'offset': offset
                    }
     url = base_url + 'users.search'
     response = requests.get(url=url, params=full_params).json()
+    pprint(response)
     return response
 
-def ids_from_users_search(response):
-    ids = list()
+
+def users_search(search_params, offset=0, count=1):
+    full_params = {**search_params, **base_params,
+                   'sort': '0',
+                   'fields': 'is_closed',
+                   'count': count,
+                   'offset': offset
+                   }
+    url = base_url + 'users.search'
+    response = requests.get(url=url, params=full_params).json()
+    if 'error' not in response:
+        pprint(response)
+        if response['response']['items']:
+            if response['response']['items'][0]['is_closed']:
+                return users_search(search_params, offset=offset+1)
+            else:
+                return response
+        else:
+            return users_search(search_params, offset=offset+1)
+    elif response['error']['error_code'] == 6:
+        time.sleep(1)
+        return users_search(search_params)
+
+
+def ids_from_users_search(response) -> list:
+    user_ids = list()
     if 'response' not in response:
         print(response)
+    if 'error' in response:
+        if response['error']['error_code'] == 5:
+            print(f'токен просрочен, перейдите по ссылке: {token_url}')
     for item in response['response']['items']:
         # print(item['id'], item['is_closed'])
         if not item['is_closed']:
-            ids.append(item['id'])
-    pprint(ids)
-    return ids
+            user_ids.append(item['id'])
+    pprint(user_ids)
+    return user_ids
 
-def get_photos(id) -> list:
-    params = {
-        'owner_id': id,
+
+def id_from_users_search(response) -> int:
+    pprint(response)
+    if 'error' in response:
+        if response['error']['error_code'] == 5:
+            print(f'токен просрочен, перейдите по ссылке: {token_url}')
+            raise Exception('token expired')
+        else:
+            raise Exception(response['error'])
+    if 'response' not in response:
+        pprint(response)
+    user_id = response['response']['items'][0]['id']
+    pprint(user_id)
+    return user_id
+
+
+def get_photos(user_id) -> list:
+    """
+    :param user_id: id человека
+    :return: список из 3 самых популярных фото человека
+    """
+    search_params = {
+        'owner_id': user_id,
         'album_id': 'profile',
         'extended': '1',
         'photo_sizes': '1'
     }
-    full_params = {**base_params, **params}
+    full_params = {**base_params, **search_params}
     url = base_url + 'photos.get'
     response = requests.get(url=url, params=full_params).json()
     if 'error' in response:
         if response['error']['error_code'] == 6:
             time.sleep(1)
-            return get_photos(id)
+            return get_photos(user_id)
     if response['response']['count'] == 0:
+        print('no photos')
         return list()
     response_photos = response['response']['items']
     response_sorted_photos = sorted(response_photos, key=lambda x: x['likes']['count'], reverse=True)
     response_top_photos = response_sorted_photos[0:3]
-    # pprint(response_top_photos)
     top_photos = list()
-    # for photo in response_top_photos:
-    #     top_photos.append(photo['sizes'][-1]['url'])
     for photo in response_top_photos:
         top_photos.append('photo' + str(photo['owner_id']) + '_' + str(photo['id']))
     return top_photos
 
-def get_multiple_photos(ids: list):
+
+def get_multiple_photos(user_ids: list):
     photos_list = list
-    for id in ids:
-        photos_list.append(get_photos(id))
+    for user_id in user_ids:
+        photos_list.append(get_photos(user_id))
     return photos_list
 
-if __name__ == '__ghain__':
+
+def database_get_countries(q: str) -> int:
+    url = base_url + 'database.getCountries'
+    response = requests.get(url, params={'q': q, 'count': 1, **base_params, 'need_all': 0}).json()
+    return response['response']['items'][0]['id']
+
+
+def database_get_cities(q: str, country_id) -> int:
+    url = base_url + 'database.getCities'
+    response = requests.get(url, params={'q': q, 'country_id': country_id, 'count': 1, **base_params, 'need_all': 0}).json()
+    return response['response']['items'][0]['id']
+
+
+if __name__ == '__hello__':
     params = {
         'q': 'Василий'
     }
@@ -76,8 +141,4 @@ if __name__ == '__ghain__':
 
 
 if __name__ == '__main__':
-    pprint(get_photos(200036391))
-    response = requests.get('https://api.vk.com/method/photos.getAlbums', params={
-        'owner_id': 200036391, **base_params, 'need_system': 1
-    }).json()
-    pprint(response)
+    pprint(database_get_cities('санкт', database_get_countries('россия')))
